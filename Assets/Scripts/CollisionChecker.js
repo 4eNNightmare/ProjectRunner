d89931c1dbba3269356519c2b45fcab17814d119
Wender Silva <wenderversaofull@hotmail.com>
2014-08-13 10:41:16 -0300

    ALteração no Script CollisionChecker
    - Houve alteração no sistema de reconhecimento de colisão do personagem,
    mundando o posicionamente e a quantidade de RayCast, além das funções
    relacionadas a reação do personagem.
    - A implementação ainda está em andamento (incompleto) .

--- Assets/Scripts/CollisionChecker.js
+++ Assets/Scripts/CollisionChecker.js
@@ -4,8 +4,11 @@ public var isGrounded : boolean;
 public var isWallHit : boolean;
 public var collisionDistanceWall : float;
 public var collisionDistanceGround : float;
+public var canClimb : boolean;
+public var climbDist : float;
 
 function Start () {
+	canClimb = false;
 	if(collisionDistanceGround == 0)
 		collisionDistanceGround = transform.localScale.y/2;
 	if(collisionDistanceWall == 0)
@@ -14,13 +17,21 @@ function Start () {
 
 function Update () {
 	var rayGroundOrigin : Vector3 = transform.position;
-	var rayWallOrigin : Vector3 = new Vector3(transform.position.x, transform.position.y - transform.localScale.y/2, transform.position.z);
+	var rayWallOriginBot : Vector3 = new Vector3(transform.position.x, transform.position.y - transform.localScale.y/2, transform.position.z);
+	var rayWallOriginTop : Vector3 = new Vector3(transform.position.x, transform.position.y + transform.localScale.y/2, transform.position.z);
+ 	
+	//var rayGroundOrigin : Vector3 = transform.position;
+	//var rayWallOrigin: Vector3 = new Vector3(transform.position.x, transform.position.y + transform.localScale.y/2, transform.position.z);
 	var groundDirection : Vector3 = Vector3.down;
 	var wallDirection : Vector3 = Vector3.right;
 	var rayGround : Ray = new Ray(rayGroundOrigin,groundDirection);
-	var rayWall : Ray = new Ray(rayWallOrigin,wallDirection);
+	var rayWallBot : Ray = new Ray(rayWallOriginBot, wallDirection);
+	var rayWallTop : Ray = new Ray(rayWallOriginTop, wallDirection);
 	var hitGround : RaycastHit;
-	var hitWall : RaycastHit;
+	var hitWallBot : RaycastHit;
+	var hitWallTop : RaycastHit;
+	
+	
 	
 	if(Physics.Raycast(rayGround, hitGround, collisionDistanceGround)){
 		if(hitGround.collider.tag == "Ground"){
@@ -31,16 +42,39 @@ function Update () {
 		isGrounded = false;
 	}
 	
-	if(Physics.Raycast(rayWall, hitWall, collisionDistanceWall)){
-		if(hitWall.collider.tag == "Ground"){
-			isWallHit = true;
-			Application.LoadLevel(Application.loadedLevel);
+	if((isGrounded) && (canClimb == false)){
+		if(Physics.Raycast(rayWallBot, hitWallBot, collisionDistanceWall)){
+			if(Physics.Raycast(rayWallTop, hitWallTop, collisionDistanceWall)){
+				if(hitWallTop.collider.tag == "Ground"){
+					if(hitWallBot.collider.tag == "Ground"){
+						isWallHit = true;
+						Application.LoadLevel(Application.loadedLevel);
+					}
+				}
+			}
+		}
+		else{
+			isWallHit = false;
 		}
 	}
 	else{
-		isWallHit = false;
-	}
+		if(canClimb == false){
+			if((Physics.Raycast(rayWallBot, hitWallBot, collisionDistanceWall)) || 
+			(Physics.Raycast(rayWallTop, hitWallTop, collisionDistanceWall))) {
+				if(hitWallBot.collider.tag == "Ground"){
+					if(hitWallTop.collider.tag == "Ground"){
+						isWallHit = true;
+						Application.LoadLevel(Application.loadedLevel);
+					}
+				}
+				else{
+				isWallHit = false;
+				}
+			}
+		}
+	} 
 	
 	Debug.DrawRay(rayGroundOrigin, groundDirection*collisionDistanceGround, Color.green);
-	Debug.DrawRay(rayWallOrigin, wallDirection*collisionDistanceWall, Color.red);
+	Debug.DrawRay(rayWallOriginBot, wallDirection*collisionDistanceWall, Color.red);
+	Debug.DrawRay(rayWallOriginTop, wallDirection*collisionDistanceWall, Color.red);
 }
