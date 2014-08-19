8c3d29fb3ce52f1c24928b0d1ecdc6cbd2354b78
Flávio <flaviocomf@hotmail.com>
2014-08-18 21:40:12 -0300

    Aprimoramento Randomize
    - Refeito a lógica do script
    - criação dos prefabs para composição dos obstaculos
    - sendo trabalhado aos poucos
    - lulz

--- Assets/Scripts/Randomizer.js
+++ Assets/Scripts/Randomizer.js
@@ -1,72 +1,79 @@
 ﻿#pragma strict
-var novoPonto 	: 	  Vector3;
-var ultimoPonto	:	  Vector3;
-var comparaDist : 	  Vector3;
-var inicialDist : 	  Vector3;
-var podeSaltar	:	  boolean;
-var objeto	 	:  GameObject;
-var contaTempo  : 		float;
-var entrePulo	:		float;
+var newPoint 	: 	  Vector3;
+var lastPoint	:	  Vector3;
+var compareDist : 	  Vector3;
+var initialDist : 	  Vector3;
+var canJump		:	  boolean;
+var object	 	:GameObject[];
+var timer  		: 		float;
+var betweenJump	:		float;
+var camada		:		  int;
+var contaCubos	:		  int;
+var contaPrimeiraCamada	:         int;
+var contaSegundaCamada	:         int;
+var contaTerceiraCamada	:         int;
+
 		
 
 function Start () {
-comparaDist = inicialDist = this.gameObject.transform.position;
-podeSaltar 	= true;
-novoPonto.x = 0;
-novoPonto.y = 0;
-novoPonto.z = 0;
-ultimoPonto = novoPonto;
-contaTempo = 0;
-entrePulo = 0;
+compareDist = initialDist = this.gameObject.transform.position;
+canJump 	= true;
+newPoint.x = 0;
+newPoint.y = 0;
+newPoint.z = 0;
+lastPoint = newPoint;
+lastPoint.x = 23; 
+lastPoint.y =-0.5;
+timer = 0;
+betweenJump = 0;
+camada = 1;
+contaPrimeiraCamada = 0;
+contaSegundaCamada = 0;
+contaTerceiraCamada = 0;
 }
 
 function Update () {
-comparaDist.x = GameObject.Find("Player").GetComponent(Transform).position.x - inicialDist.x;
+compareDist.x = GameObject.Find("Player").GetComponent(Transform).position.x - initialDist.x;
 
-if(contaTempo >= 0.5)
+if(timer >= 0.05 && contaCubos < 151)
 {
 
-	ultimoPonto.y = -0.29;
-	novoPonto.y =	 -0.29;
-	novoPonto.x = entrePulo + comparaDist.x;
-	
-	
-	if(Random.value > 0.2)
-	{
-		Instantiate(objeto,novoPonto,transform.rotation);
-		novoPonto.x = comparaDist.x + 1 + entrePulo;
-		Instantiate(objeto,novoPonto,transform.rotation);
-		entrePulo += 0;
-		podeSaltar = true;
-	}
-	
-	else 
-	{
-		if(podeSaltar)
-		{
-			novoPonto.x = comparaDist.x +3.5 + entrePulo;
-			Instantiate(objeto,novoPonto,transform.rotation);
-			novoPonto.x = comparaDist.x +4.5 + entrePulo;
-			Instantiate(objeto,novoPonto,transform.rotation);
-			entrePulo += 3.5;
-			if(Random.value > 0.6)
-			{
-				podeSaltar = false;
-			}
-		}
-		else{
-			Instantiate(objeto,novoPonto,transform.rotation);
-			novoPonto.x = comparaDist.x + 1 + entrePulo;
-			Instantiate(objeto,novoPonto,transform.rotation);
-			entrePulo += 0;
-		}
-	}
-			
-	contaTempo = 0;
+	//newPoint.y =	 -0.5;
+	//newPoint.x = betweenJump + compareDist.x;	
+	DrawInstance(EscolheObjeto(camada));			
+	timer = 0;
 }	
 
 }
 
 function FixedUpdate(){
-contaTempo += Time.deltaTime;
+timer += Time.deltaTime;
+}
+
+function DrawInstance (tipo : int) : void
+{
+		camada = tipo;
+		Instantiate(object[tipo],lastPoint,transform.rotation);
+		lastPoint.x += 1 + betweenJump;
+		betweenJump += 0;
+		contaCubos += 1;
+		canJump = true;
+}
+
+
+function EscolheObjeto(camada)
+{
+if(Random.value > 0.5)
+{
+	camada = 1;
+	lastPoint.y = -0.25;
+
+}
+else{
+	camada = 2;
+	lastPoint.y = 0;
+
+}
+return camada;
+
 }
