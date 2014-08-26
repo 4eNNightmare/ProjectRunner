a12bfb1427cecfd392ed8d96bd7c173c9c249382
ramongq01 <ramongq01@hotmail.com>
2014-08-26 19:20:31 -0300

    Revert "Revert "Criação do Swipe""
    This reverts commit cde4e1b514f5ffa810a1b9aa464259bb3453aced.

---
+++ Assets/Scripts/TrailTouch.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+
+function Update () {
+	//var ray : Ray = Camera.main.ScreenPointToRay (TouchSwipe.touch.position);
+	switch (TouchSwipe.touch.phase){
+			case TouchPhase.Moved:
+				var ray : Ray = Camera.main.ScreenPointToRay (TouchSwipe.touch.position);
+				var pos : Vector3 = ray.GetPoint(5);
+				transform.position = pos;
+			break;
+	}
+}
