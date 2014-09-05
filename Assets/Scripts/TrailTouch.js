9995ac4ab5b7f4c97fc771bba280b21a5f40adf6
ramongq01 <ramongq01@hotmail.com>
2014-09-04 23:02:25 -0300

    Criação do MouseControler e aprimoramento do Swipe
    Aprimoração do Swipe e Traill e adição co controle pelo mouse denominado
    por MouseControler

--- Assets/Scripts/TrailTouch.js
+++ Assets/Scripts/TrailTouch.js
@@ -1,11 +1,11 @@
 ﻿#pragma strict
 
 function Update () {
-	switch (GameObject.Find("Player/Main Camera").GetComponent(TouchSwipe).touch.phase){
-			case TouchPhase.Moved:
-				var ray : Ray = GameObject.Find("cameraTrail").camera.ScreenPointToRay(Input.mousePosition);
-				var pos : Vector3 = ray.GetPoint(5);
-				transform.position = pos;
-			break;
+	if(GameObject.Find("Player/Main Camera").GetComponent(TouchSwipe).touch.phase == TouchPhase.Moved)
+		var ray : Ray = GameObject.Find("cameraTrail").camera.ScreenPointToRay(Input.mousePosition);
+	else if (Input.GetMouseButton(0))
+		var ray : Ray = GameObject.Find("cameraTrail").camera.ScreenPointToRay(Input.mousePosition);
+	var pos : Vector3 = ray.GetPoint(5);
+	transform.position = pos;
 	}
 }
