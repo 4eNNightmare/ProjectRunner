4cec0d07432157b479cc8ae05c78ec06a90405c3
ramongq01 <ramongq01@hotmail.com>
2014-11-04 08:55:13 -0200

    Aprimoramento Pause e Criação de menus
    Pause: (Troca das imagens, inserção do GUISkin no botao e
    posicionamento)
    Menus: (Criação basica de uma nova cena e tres botoes novo jogo,
    continuar e sair)

---
+++ Assets/Scripts/MenuP.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+
+var posiX: float;
+var posiY: float;
+
+var altura: float;
+var largura: float;
+
+var newgame: boolean;
+var botao: boolean;
+
+static public var faseAtualNumber : int;
+
+function Start () {
+altura = 40;
+largura = 150;
+
+posiX = Screen.width/2 - largura/2;
+posiY = Screen.height/2 - altura/2;
+
+newgame = true;
+botao = true;
+}
+function Update () {
+}
+function OnGUI(){
+	if (GUI.Button(new Rect(posiX, posiY-50, largura, altura), "fase1")){
+		/*if(newgame){
+			GUI.Box(Rect(posiX, posiY-50, largura, altura),"Todo progresso sera \n perdido deseja continuar?");
+			if (GUI.Button(new Rect(posiX, posiY-50, largura, altura), "Sim")){
+    			*/Application.LoadLevel(1);
+				faseAtualNumber = 1;
+			/*}
+			if (GUI.Button(new Rect(posiX, posiY-50, largura, altura), "Nao")){
+    			Application.LoadLevel(2);
+				faseAtualNumber = 2;
+			}
+    	}*/
+    }
+    if (GUI.Button(new Rect(posiX, posiY, largura, altura), "fase2")){
+			Application.LoadLevel(2);
+    }
+    if(GUI.Button(Rect(posiX, posiY+50, largura, altura), "Sair")){
+    	Application.Quit();
+	}
+}
--- Assets/Scripts/Pause.js
+++ Assets/Scripts/Pause.js
@@ -6,34 +6,46 @@ var posiY: float;
 var altura: float;
 var largura: float;
 
-var perSkin: GUISkin;
-var perSkin2: GUISkin;
-
 var controlePause: boolean;
+var botao: boolean;
+
+var pausar: Texture2D;
+var continuar: Texture2D;
+
+var alteraBotao: GUISkin;
 
 function Start () {
-
-altura = 50;
-largura = 150;
+altura = 100;
+largura = 100;
 
 posiX = Screen.width/2 - largura/2;
 posiY = Screen.height/2 - altura/2;
 
 controlePause = true;
+botao = true;
 }
 function Update (){
 }
 function OnGUI(){
-	if (GUI.Button(new Rect(posiX, posiY-210, largura, altura), "Pause")){
-		if (controlePause){
-    		Time.timeScale = 0;
-    		controlePause = false;
+	if(botao){
+		GUI.skin=alteraBotao;
+		if (GUI.Button(new Rect(Screen.width-170, 10, largura, altura), pausar)){
+			if (controlePause){
+    			Time.timeScale = 0;
+    			controlePause = false;
+    			botao = false;
+    		}
     	}
     }
-	if(controlePause == false){
-		if(GUI.Button(Rect(posiX, posiY-110, largura, altura), "Continuar")){
-			Time.timeScale = 1;
-			controlePause = true;
+    if(botao == false){
+		if(controlePause == false){
+			GUI.Box(Rect(0, 0, Screen.width, Screen.height)," ");
+			GUI.skin=alteraBotao;
+			if(GUI.Button(Rect(Screen.width-170, 10, largura, altura), continuar)){
+				Time.timeScale = 1;
+				controlePause = true;
+				botao = true;
+			}
 		}
 	}
 }
