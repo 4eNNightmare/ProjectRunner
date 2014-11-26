6aad3089b8a46ad8ef52951f3d71d3bdc4a36c39
Wender Silva <wenderversaofull@hotmail.com>
2014-11-26 02:35:32 -0200

    Personagem + Animação
    -Adicão do Modelo loe poly do personagem (sem textura)
    -Adição da animação do Personagem

--- Assets/Scripts/AnimationController.js
+++ Assets/Scripts/AnimationController.js
@@ -9,16 +9,28 @@ function Start () {
 }
 
 function Update () {
+	
 
 	if(GetComponent(CollisionChecker).isGrounded){
+		GameObject.FindWithTag("Character").animation.CrossFadeQueued("run", 0.10, QueueMode.CompleteOthers);
 		transform.renderer.material.color = new Color(0,0,1,1);
+		
+		
 	}
 	else{
+		
 		if(rigidbody.velocity.y < 0){
-			transform.renderer.material.color = new Color(1,0,0,1);
+			if(GetComponent(PlayerController).action == actionState.DownwardStrike){
+				GameObject.FindWithTag("Character").animation.CrossFadeQueued("DownWard", 0.02, QueueMode.PlayNow);
+			}
+			else{
+				GameObject.FindWithTag("Character").animation.CrossFadeQueued("Jump_Down", 0.02, QueueMode.CompleteOthers);
+				transform.renderer.material.color = new Color(1,0,0,1);
+			}
 		}
 		else{
-			transform.renderer.material.color = new Color(1,1,0,1);
+				GameObject.FindWithTag("Character").animation.CrossFadeQueued("JumpAir_Up", 0.02, QueueMode.PlayNow);
+				transform.renderer.material.color = new Color(1,1,0,1);
 		}
 	}
 	
@@ -27,9 +39,10 @@ function Update () {
 	}
 	
 	if(GetComponent(PlayerController).action == GetComponent(PlayerController).action.Roll){
+		GameObject.FindWithTag("Character").animation.Play("roll");
 		transform.renderer.material.color = new Color(0,0,0,0);
-		GameObject.Find("Player/PlayerCollider").transform.renderer.material.color = new Color(0,0,1,1);
-		GameObject.Find("Player/PlayerCollider").transform.renderer.enabled = true;
+		//GameObject.Find("Player/PlayerCollider").transform.renderer.material.color = new Color(0,0,1,1);
+		//GameObject.Find("Player/PlayerCollider").transform.renderer.enabled = true;
 	}
 	else{
 		GameObject.Find("Player/PlayerCollider").transform.renderer.enabled = false;
