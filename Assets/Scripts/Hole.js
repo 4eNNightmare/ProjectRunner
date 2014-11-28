403ffcdbf86e72b208cfddf29cba4da137b43901
Flávio <flaviocomf@hotmail.com>
2014-11-28 12:25:57 -0200

    Correção de bugs 2
    -os buracos conseguem matar o jogador que cair sem se colidir com algo.

---
+++ Assets/Scripts/Hole.js
@@ -0,0 +1,7 @@
+﻿#pragma strict
+
+function OnTriggerEnter (other : Collider) {
+	if(other.name == 'Player'){
+		GameObject.Find("Player").GetComponent(GameStateController).gameState = GameObject.Find("Player").GetComponent(GameStateController).gameState.GameOver;
+	}
+}
